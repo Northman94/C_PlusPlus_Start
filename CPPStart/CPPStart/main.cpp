03dbda526baff5af03f67dc59e133f564ec8adb9
Northman94 <zhandarm2000@gmail.com>
2020-01-28 20:47:39 +0200

    Carpet cleaning price proj creation

---
+++ CPPStart/CPPStart/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n";
+	
+    cout << "Hello, wellcome to carpet cleaning service!" << endl;
+    
+    
+    cout << "\n";
+	return 0; 
+}
