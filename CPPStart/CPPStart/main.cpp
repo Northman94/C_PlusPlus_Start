cf7451e3a2c017abc0a27d2cd514a88a1e826ec6
Northman94 <zhandarm2000@gmail.com>
2020-01-28 21:52:23 +0200

    Intro & birst IO

--- CPPStart/CPPStart/main.cpp
+++ CPPStart/CPPStart/main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "\n";
+	cout << "\n";
 	
-    cout << "Hello, wellcome to carpet cleaning service!" << endl;
-     
-    
-    cout << "\n";
+	cout << "Hello, wellcome to carpet cleaning service!" << endl;
+	cout << "It's 30 $ per room & 6 % tax rate." << endl;
+	cout << "\nHow many room would you like to be cleaned ?" << endl; 
+	
+	int number_of_rooms = 0;
+	cin >> number_of_rooms;
+	
+	cout << "estimate for carpet cleaning service is:" << number_of_rooms << endl;
+	
+	cout << endl;
 	return 0; 
 }
