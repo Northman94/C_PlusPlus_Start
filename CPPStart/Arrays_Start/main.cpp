5084423e69b72467823ebc42a814792c306bbc04
Northman94 <zhandarm2000@gmail.com>
2020-02-17 13:36:13 +0200

    Arrays final save commit

---
+++ CPPStart/Arrays_Start/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int  test_scores  [5] ;
+
+	cout << “\n First score at index 0: ” << test_scores [0] << endl;
+	cout <<  “\n Second score at index 1: ” << test_scores [1] << endl;
+	cout << “\n Third score at index 2: ” << test_scores [0] << endl;
+	cout <<  “\n Fourth score at index 3: ” << test_scores [1] << endl;
+	cout <<  “\n Fifth score at index 4: ” << test_scores [1] << endl;
+}
--- CPPStart/CPPStart/main.cpp
+++ CPPStart/CPPStart/main.cpp
@@ -28,6 +28,8 @@ int main()
 }
 */
 
+
+/*
 // Better replace literal constants with declared constants:
 
 #include <iostream>
@@ -60,4 +62,25 @@ int main()
 	
 	cout << endl;
 	return 0; 
+}
+*/
+
+
+//Arrays
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int  test_scores  [5];
+
+	cout <<  "\n First score at index 0: " << test_scores [0] << endl;
+	cout <<  "\n Second score at index 1: " << test_scores [1] << endl;
+	cout <<  "\n Third score at index 2: " << test_scores [0] << endl;
+	cout <<  "\n Fourth score at index 3: " << test_scores [1] << endl;
+	cout <<  "\n Fifth score at index 4: " << test_scores [1] << endl;
+	
+	cout << test_scores;
+	
+	return 0;
 }
