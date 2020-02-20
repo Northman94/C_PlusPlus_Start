21d7bca2f3e6d72f1ba5fe36e1cf646e95f616d1
Northman94 <zhandarm2000@gmail.com>
2020-02-20 20:54:19 +0200

    2D Vectors

--- CPPStart/Vectors/main.cpp
+++ CPPStart/Vectors/main.cpp
@@ -37,3 +37,61 @@ int main()
 	return 0;
 }*/
 
+/*
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	vector <int> array_addition {100, 90, 80};
+	
+	array_addition.push_back (70);
+	
+	cin >> array_addition[2];
+	cout << array_addition[2] << endl;
+	
+	cin >> array_addition.at (3);
+	cout << array_addition.at (3) << endl;
+	
+	cout << "Vector Size: " << array_addition.size() << endl;
+	
+	cout << "\n";
+	return 0;
+}
+*/
+
+
+
+// 2D Vector = vector of vectors
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	vector <vector <int>> array_of_arrays
+	{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12}
+	};
+	
+	//Using Array synthax:
+	cout << array_of_arrays[0][0] << endl;
+	cout << array_of_arrays[0][1] << endl;
+	cout << array_of_arrays[0][2] << endl;
+	cout << array_of_arrays[0][3] << endl;
+	//Using Vector synthax:
+	cout << array_of_arrays.at(1).at(0) << endl;
+	cout << array_of_arrays.at(1).at(1) << endl;
+	cout << array_of_arrays.at(1).at(2) << endl;
+	cout << array_of_arrays.at(1).at(3) << endl;
+	
+	cout << "\n";
+	return 0;
+}
+
+
